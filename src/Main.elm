a5e635b80398f9ee58d5dacc4601c76708061aa3
James Gary <mrjamesgary@gmail.com>
2018-11-03 02:07:17 -0700

    movin and groovin

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,30 @@
 module Main exposing (main)
 
-import Browser exposing (Document, document)
+import Browser
+import Browser.Events
 import Color
 import Game.TwoD as GameTwoD
 import Game.TwoD.Camera as GameTwoDCamera
 import Game.TwoD.Render as GameTwoDRender
 import Html
 import Html.Attributes
+import Json.Decode as Decode
+import Set exposing (Set)
 
 
 main =
-    document
+    Browser.document
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
+
+
+type alias Vec2 =
+    { x : Float
+    , y : Float
+    }
 
 
 type alias Flags =
@@ -24,39 +33,106 @@ type alias Flags =
 
 type alias Model =
     { hero : Hero
+    , keysPressed : Set Key
     }
 
 
 type alias Hero =
-    { loc : ( Float, Float )
+    { loc : Vec2
     }
 
 
+type alias Key =
+    String
+
+
 type Msg
-    = NoOp
+    = KeyDown String
+    | KeyUp String
+    | Tick Float
 
 
 init : Flags -> ( Model, Cmd msg )
 init flags =
-    ( { hero = { loc = ( -2, 0 ) }
+    ( { hero =
+            { loc = { x = -2, y = 0 }
+            }
+      , keysPressed = Set.empty
       }
     , Cmd.none
     )
 
 
+vec2ToTuple : Vec2 -> ( Float, Float )
+vec2ToTuple { x, y } =
+    ( x, y )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Tick delta ->
+            ( { model
+                | hero =
+                    model.hero
+                        |> (\hero ->
+                                { hero
+                                    | loc =
+                                        { x =
+                                            if
+                                                Set.member "ArrowLeft" model.keysPressed
+                                                    || Set.member "a" model.keysPressed
+                                            then
+                                                hero.loc.x - 1
+
+                                            else if
+                                                Set.member "ArrowRight" model.keysPressed
+                                                    || Set.member "d" model.keysPressed
+                                            then
+                                                hero.loc.x + 1
+
+                                            else
+                                                hero.loc.x
+                                        , y =
+                                            if
+                                                Set.member "ArrowUp" model.keysPressed
+                                                    || Set.member "w" model.keysPressed
+                                            then
+                                                hero.loc.y + 1
+
+                                            else if
+                                                Set.member "ArrowDown" model.keysPressed
+                                                    || Set.member "s" model.keysPressed
+                                            then
+                                                hero.loc.y - 1
+
+                                            else
+                                                hero.loc.y
+                                        }
+                                }
+                           )
+              }
+            , Cmd.none
+            )
+
+        KeyUp str ->
+            ( { model | keysPressed = Set.remove str model.keysPressed }, Cmd.none )
+
+        KeyDown str ->
+            ( { model | keysPressed = Set.insert str model.keysPressed }, Cmd.none )
+                |> Debug.log str
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Browser.Events.onKeyDown (Decode.map KeyDown (Decode.field "key" Decode.string))
+        , Browser.Events.onKeyUp (Decode.map KeyUp (Decode.field "key" Decode.string))
+        , Browser.Events.onAnimationFrameDelta Tick
+        ]
 
 
-view : Model -> Document Msg
+view : Model -> Browser.Document Msg
 view model =
     let
         canvasWidth =
@@ -83,7 +159,7 @@ view model =
             GameTwoDRender.shape
                 GameTwoDRender.rectangle
                 { color = Color.black
-                , position = model.hero.loc
+                , position = model.hero.loc |> vec2ToTuple
                 , size = ( 1, 1 )
                 }
     in
